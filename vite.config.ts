f2a6fb09ee3b938d1656658a08108ce15c1e7fab
Louis <louis.caille@hubup.fr>
2024-11-14 23:22:30 +0100

    Set build to public folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+function publicPath() {
+  if (process.env.CI_PAGES_URL) {
+    return new URL(process.env.CI_PAGES_URL).pathname
+  } else {
+    return '/'
+  }
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  base: publicPath(),
+  build: {
+    outDir: 'public',
   },
 })
